0e5c6f264a65e1ce08ee08258762eb380abf682f
Fernando Ferreira <nandoferreira@users.noreply.github.com>
2019-02-06 14:57:21 -0200

    * added via GITHUB Desktop

--- Lib/Script/st_preview.py
+++ Lib/Script/st_preview.py
@@ -4,7 +4,7 @@
 #
 # Created by: PyQt5 UI code generator 5.11.3
 #
-# WARNING! All changes made in thi file will be lost!
+# WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
--- README.md
+++
@@ -1,3 +0,0 @@
-# QT Stylesheet Designer
-
-### A Simple QT Stylesheet designer for PYQT/QT Projects made with PYQT
