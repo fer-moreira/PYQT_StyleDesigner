4d5e2e164c8873af5ed519905c758b4b313dd79c
Fernando Ferreira <34872851+zisongbr@users.noreply.github.com>
2019-03-08 10:25:10 -0300

    Added import button

--- editor.py
+++ editor.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QApplication, QMainWindow,QMenuBar,QAction
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog,QMessageBox
 from PyQt5 import QtGui
 
 from Lib.Script.st_preview import Ui_preview
@@ -29,8 +29,23 @@ class Editor(QMainWindow,Ui_editor):
         _style = str(self.input.toPlainText())
         styleFile.write(_style)
         styleFile.close()
+        
     def import_style_to_input (self):
-        print("* DEV")
+        
+        try:
+            fname = QFileDialog.getOpenFileName(self, 'Load Style From file', 'Stylesheet',"Select file (*.txt *.css *.qss)")
+            selectFilePath = str(fname[0])
+            fileopen = open(r'%s'%selectFilePath,'r')
+            _content = str(fileopen.read())
+            fileopen.close()
+
+            dialog = QMessageBox.question(self, "WARNING", "Load selected file ?",QMessageBox.Yes,QMessageBox.No)
+
+            if dialog == QMessageBox.Yes:
+                self.input.setPlainText(_content)
+        except FileNotFoundError:pass
+
+
     def load_style_from_file (self):
         styleFile = open(r'assets\_stylesheet.css','r')
         _style = str(styleFile.read())
